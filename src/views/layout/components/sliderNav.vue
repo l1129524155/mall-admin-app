98f93e45472a6b807cde2be5c70458cc8db6a6e6
lijian <1129524155@qq.com>
2020-11-14 15:43:45 +0800

    组件化

---
+++ src/views/layout/components/sliderNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="main-header">
+    <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadcrumb">
+      <template>
+        <a-breadcrumb>
+          <a-breadcrumb-item>首页</a-breadcrumb-item>
+          <a-breadcrumb-item>
+            <a href>统计</a>
+          </a-breadcrumb-item>
+        </a-breadcrumb>
+      </template>
+    </div>
+    <ul class="user-info">
+      <li>
+        欢迎123
+        <a-icon type="down" />
+      </li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed')
+    }
+  }
+};
+</script>
